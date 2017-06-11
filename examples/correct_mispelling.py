f5aa9e2cc65183cc1cafbb16597cc3ba84e8398c
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-11 20:42:31 +0200

    Added print to original sentence

--- examples/correct_mispelling.py
+++ examples/correct_mispelling.py
@@ -1,6 +1,7 @@
 import luvina.backend as luv
 
-sentence = 'the languge is very complicated'
+sentence = 'languge is veri complicaated'
+print(sentence)
 tokens = luv.tokenize(sentence)
 corrected_sentence= [luv.correct_misspelling(token) for token in tokens]
 print(corrected_sentence)
