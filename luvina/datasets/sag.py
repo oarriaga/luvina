9d1f11cf64fa0aa5a6b9c46b7089f084eed6e650
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-08-10 18:27:45 +0200

    Added get_data function for short question answering dataset

--- luvina/datasets/sag.py
+++ luvina/datasets/sag.py
@@ -1,18 +1,20 @@
 import pandas as pd
+import glob
+from os.path import basename
 
 
-def get_data():
+def _get_string_data():
     data_path = '../datasets/short_answer_grading_v2/raw/'
     questions_filename = data_path + 'questions'
     teacher_answers_filename = data_path + 'answers'
     student_answers_filename = data_path + 'all'
-    questions = read_file(questions_filename)
-    teacher_answers = read_file(teacher_answers_filename)
-    student_answers = read_file(student_answers_filename)
+    questions = _read_file(questions_filename)
+    teacher_answers = _read_file(teacher_answers_filename)
+    student_answers = _read_file(student_answers_filename)
     return questions, teacher_answers, student_answers
 
 
-def read_file(filename):
+def _read_file(filename):
     data_file = open(filename, 'r', encoding='latin-1')
     texts = []
     data_ids = []
@@ -34,4 +36,60 @@ def read_file(filename):
     return data_frame
 
 
-questions, teacher_answers, student_answers = get_data()
+def _get_scores():
+    data_path = '../datasets/short_answer_grading_v2/scores/'
+    score_paths = glob.glob(data_path + '*')
+    scores = dict()
+    for scores_path in score_paths:
+        score_files = glob.glob(scores_path + '/*')
+        question_scores = []
+        for score_file in score_files:
+            file_scores = []
+            _score_file = open(score_file, 'r')
+            for line in _score_file:
+                file_scores.append(float(line))
+            question_scores.append(file_scores)
+        scores_key = basename(scores_path)
+        scores[scores_key] = question_scores
+    return scores
+
+
+def _convert_to_dictionary(string_data):
+    data = dict()
+    for arg, key in enumerate(string_data.id.tolist()):
+        data[key] = string_data.text.iloc[arg]
+    return data
+
+
+def get_data():
+    questions, teacher_answers, student_answers = _get_string_data()
+    scores = _get_scores()
+    teacher_answers = _convert_to_dictionary(teacher_answers)
+    teacher_answers_list = []
+    student_answers_list = []
+    scores_1 = []
+    scores_2 = []
+    scores_3 = []
+    keys = student_answers.id.tolist()
+    for arg, key in enumerate(keys):
+        teacher_answers_list.append(teacher_answers[key])
+        student_answers_list.append(student_answers.text.iloc[arg])
+        scores_list = scores[key]
+        scores_1.append(scores_list[0])
+        scores_2.append(scores_list[1])
+        scores_3.append(scores_list[2])
+    data_1 = pd.DataFrame({'teacher': teacher_answers_list,
+                           'student': student_answers_list,
+                           'score': scores_1})
+
+    data_2 = pd.DataFrame({'teacher': teacher_answers_list,
+                           'student': student_answers_list,
+                           'score': scores_2})
+
+    data_3 = pd.DataFrame({'teacher': teacher_answers_list,
+                           'student': student_answers_list,
+                           'score': scores_3})
+
+    data_frames = [data_1, data_2, data_3]
+    data = pd.concat(data_frames, axis=0)
+    return data
