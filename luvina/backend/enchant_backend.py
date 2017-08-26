434823a9c1501489700beb94620429be2207844d
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-08-26 21:13:53 +0200

    Added enchant_backend with dictionary utils

---
+++ luvina/backend/enchant_backend.py
@@ -0,0 +1,29 @@
+import enchant
+
+# -------------------------------------------------------------------
+# ENCHANT GLOBAL VARIABLES
+# -------------------------------------------------------------------
+spell_dictionary = enchant.Dict('en')
+# -------------------------------------------------------------------
+
+
+def in_dictionary(token):
+    """ checks if a token is found in the dictionary or not
+    args:
+        token: string
+    returns:
+        boolean determining if the token was found in dictionary
+    """
+    return spell_dictionary.check(token)
+
+
+def suggest_words(token):
+    """ suggest similar tokens
+    args:
+        token: string
+    returns:
+        list of strings containing similar tokens
+    """
+    return spell_dictionary.suggest(token)
+
+
