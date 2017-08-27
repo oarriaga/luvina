cf495f4d2906441cf73b3734dbae74e45049f449
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-08-27 14:40:39 +0200

    Modified backend init calls

--- luvina/__init__.py
+++ luvina/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
 from . import backend
-from . import keras
 
 __version__ = '0.0.1'
--- luvina/backend/__init__.py
+++ luvina/backend/__init__.py
@@ -1,17 +1,5 @@
-from __future__ import absolute_import
-from __future__ import print_function
-import sys
-
-_BACKEND = 'nltk'
-if _BACKEND == 'nltk':
-    sys.stderr.write('Using NLTK backend.\n')
-    from .nltk_backend import *
-else:
-    raise ValueError('Unknown backend: ' + str(_BACKEND))
-
-
-def backend():
-    """Public method for determining
-    the current backend.
-    """
-    return _BACKEND
+# from .common import *
+# from .enchant_backend import *
+# from .nltk_backend import *
+# from .spacy_backend import *
+from .backend import *
